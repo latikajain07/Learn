a561874faf95e2b5551355ae77be4a784729e85a
Latika <Latikajain@live.com>
2023-04-12 20:39:30 +0530

    committed

---
+++ src/main/java/com/tap/App.java
@@ -0,0 +1,13 @@
+package com.tap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
